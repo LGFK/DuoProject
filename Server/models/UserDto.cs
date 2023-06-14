cd1b25de2f87ecfd464383ef374a370a1baf56ac
Eneibal <nemesistotv1995@gmail.com>
2023-06-14 18:57:58 +0300

    Add server

---
+++ Server/models/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.models;
+internal class UserDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+
+}
