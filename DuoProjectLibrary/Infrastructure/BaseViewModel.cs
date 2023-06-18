636ff833af2e327dd1c8644db6ab98be504ba012
E-one <potlod13@gmail.com>
2023-06-18 17:30:45 +0300

    Project Was Created

---
+++ DuoProjectLibrary/Infrastructure/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuoProjectLibrary.Infrastructure
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string _propName="")
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(_propName));
+        }
+    }
+}
