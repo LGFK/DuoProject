fbbfda05aeb240feb1e93da237bd7969f7fd994e
E-one <potlod13@gmail.com>
2023-07-11 15:52:15 +0300

    EditingWindow was added , also book class was modified due to bad db creation

---
+++ DuoProjectLibrary/Infrastructure/ApplicationState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuoProjectLibrary.Infrastructure
+{
+    public class ApplicationState
+    {
+        private static IModalWindowService _modalWindowService;
+        public static IModalWindowService ModalWindowService
+        {
+            get
+            {
+                return _modalWindowService ?? (_modalWindowService = new EditingWindowService());
+            }
+        }
+    }
+}
---
+++ DuoProjectLibrary/MVVM/View/EditingWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DuoProjectLibrary.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for EditingWindow.xaml
+    /// </summary>
+    public partial class EditingWindow : Window
+    {
+        public EditingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+    }
+}
